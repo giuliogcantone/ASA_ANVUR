e825154325d70fc1a6b8ffb266ab926647d35937
GGCantone <prgcan@gmail.com>
2023-05-22 00:55:34 +0200

    yay

--- prova.R
+++ prova.R
@@ -10,33 +10,14 @@ db$works[1:100,] %>%
                  discard, ~length(.x) == 0)) -> prova2
 
 
-db$works[1:100,] %>%
-  mutate(internal_references =
-           map(references,
-               ~str_extract_all(.,id_list)
-           )) -> prova2
+db$works %>%
+  unnest(references) %>%
+  transmute(from = id,
+            to = references,
+            from_j = journal) %>%
+  filter(to %in% db$works$id) %>%
+  left_join(.,db$works %>% select(to_j = journal,id),
+            by = c("to" = "id")
+            )-> edgelist
 
-str_c(db$works$id,
-      collapse = "|") -> id_list
-
-str_c(db$works$id
-
-prova2 <- db$works[1:10,] %>%
-  mutate(internal_references = sapply(references, function(x) {
-    str_extract_all(x, str_c(db$works$id, collapse = "|"))
-  }))
-
-db$works[1:10,] %>%
-  mutate(internal_references =
-           str_remove_all(
-             references,
-             str_c("?!(",
-                    str_c(db$works$id, collapse = "|"),
-                    ")\\w+")
-             )
-         ) -> prova2
-
-
-prova2$internal_references[1]
-
-db$works$i
+db$works$id
